365207ea7bb72133ef9526f8b8c9d34c4d049052
Søren Sjørup <soren@zoren.dk>
2024-04-26 13:13:21 +0200

    more niceness

--- vscode-extension.js
+++ vscode-extension.js
@@ -1,16 +1,38 @@
 const vscode = require('vscode')
+const { SemanticTokensLegend, SemanticTokensBuilder, Position, Range, languages } = vscode
+
+const tokenTypes = ['class', 'interface', 'enum', 'function', 'variable']
+const tokenModifiers = ['declaration', 'definition', 'defaultLibrary', 'static', 'local']
+const legend = new SemanticTokensLegend(tokenTypes, tokenModifiers)
 
 /**
  * @param {vscode.ExtensionContext} context
  */
 function activate(context) {
+  console.log('starting wuns lang extension')
+  {
+    {
+      const { commands, window } = vscode
+
+      let disposable = commands.registerCommand('wunslang.helloWorld', function () {
+        window.showInformationMessage('wuns [here] 007 [if [quote 32]]')
+      })
+      context.subscriptions.push(disposable)
+    }
+  }
+
+  const provider = {
+    provideDocumentSemanticTokens(_document) {
+      const tokensBuilder = new SemanticTokensBuilder(legend)
+      tokensBuilder.push(new Range(new Position(1, 1), new Position(1, 5)), 'variable', ['local'])
+      return tokensBuilder.build()
+    },
+  }
+
+  const selector = { language: 'wuns', scheme: 'file' }
+
+  context.subscriptions.push(languages.registerDocumentSemanticTokensProvider(selector, provider, legend))
   console.log('Congratulations, your extension "wunslang" is now active!')
-
-  let disposable = vscode.commands.registerCommand('wunslang.helloWorld', function () {
-    vscode.window.showInformationMessage('wuns [here] 007 [if [quote 32]]')
-  })
-
-  context.subscriptions.push(disposable)
 }
 
 function deactivate() {}
