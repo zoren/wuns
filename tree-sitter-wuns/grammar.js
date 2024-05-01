bad92bab52adb2b54f3fe6f83369c7ea48f1f36e
Søren Sjørup <soren@zoren.dk>
2024-05-01 16:01:41 +0200

    drop special

--- tree-sitter-wuns/grammar.js
+++ tree-sitter-wuns/grammar.js
@@ -7,8 +7,8 @@ module.exports = grammar({
     // todo look into https://tree-sitter.github.io/tree-sitter/creating-parsers#keyword-extraction
     _special_form: () => choice('quote', 'if', 'let', 'loop', 'cont', 'func', 'macro'),
     // _special_form: ($) =>  seq('[', choice('quote', 'if', 'let', 'loop', 'cont', 'func', 'macro'), repeat($._form), ']'),
-    list_special_form: ($) => seq('[', $._special_form, repeat($._form), ']'),
+    // list_special_form: ($) => seq('[', $._special_form, repeat($._form), ']'),
     list: ($) => seq('[', repeat($._form), ']'),
-    _form: ($) => choice($.word, $.list_special_form, $.list),
+    _form: ($) => choice($.word, $.list),
   },
 })
