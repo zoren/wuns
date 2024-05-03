6e1fd0922b252c7f8443a4883bdb31445ea48df7
Søren Sjørup <soren@zoren.dk>
2024-05-03 08:58:43 +0200

    remove noise

--- tree-sitter-wuns/grammar.js
+++ tree-sitter-wuns/grammar.js
@@ -4,10 +4,6 @@ module.exports = grammar({
   rules: {
     source_file: ($) => repeat($._form),
     word: () => /[a-z0-9.=-]+/,
-    // todo look into https://tree-sitter.github.io/tree-sitter/creating-parsers#keyword-extraction
-    _special_form: () => choice('quote', 'if', 'let', 'loop', 'cont', 'func', 'macro'),
-    // _special_form: ($) =>  seq('[', choice('quote', 'if', 'let', 'loop', 'cont', 'func', 'macro'), repeat($._form), ']'),
-    // list_special_form: ($) => seq('[', $._special_form, repeat($._form), ']'),
     list: ($) => seq('[', repeat($._form), ']'),
     _form: ($) => choice($.word, $.list),
   },
