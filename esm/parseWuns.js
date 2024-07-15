6c415dd7ae8ebe130b3e93def91c5115f7a1fa6a
Søren Sjørup <soren@zoren.dk>
2024-07-15 13:10:33 +0200

    some lex

--- esm/parseWuns.js
+++ esm/parseWuns.js
@@ -57,6 +57,8 @@ parseEvalFile('np.wuns')
 const { getVarObject } = context
 apply(getVarObject('bump-alloc-init').getValue())
 const parse = getVarObject('parse').getValue()
+const lexOne = getVarObject('lex-one').getValue()
+const bumpAlloc = getVarObject('bump-alloc').getValue()
 
 // for (const input of ['']) {
 //   console.log('evaluating:', input)
@@ -66,10 +68,28 @@ const parse = getVarObject('parse').getValue()
 //   callClosure(parse, [handle])
 // }
 
-for (const input of ['', 'abc', 'abc 123']) {
-  const handle = byteInputHandles.size
-  byteInputHandles.set(String(handle), { bytes: textEncoder.encode(input), offset: 0 })
-  apply(parse, handle, 64)
+const bufferWord = apply(bumpAlloc, 64)
+// console.log('buffer:', bufferWord)
+const bufferNum = parseInt(bufferWord)
+const memory = getMemory(0)
+
+for (const input of ['', 'abc', 'abc 123', 'fun-with-dash 345 ILL', '[list 1 2]']) {
+  const buffer = new Uint8Array(memory.buffer, bufferNum, 64)
+  const { read, written } = textEncoder.encodeInto(input, buffer)
+  let cur = bufferNum
+  console.log({ input })
+
+  while (true) {
+    if (cur >= bufferNum + written) break
+    const [kind, lw] = apply(lexOne, cur, cur + written)
+    const length = parseInt(lw)
+    console.log({ kind: String(kind), length })
+    cur += (length)
+  }
+  console.log('')
+  // const handle = byteInputHandles.size
+  // byteInputHandles.set(String(handle), { bytes: textEncoder.encode(input), offset: 0 })
+  // apply(parse, handle, 64)
 }
 
 // const commandLineArgs = process.argv.slice(2)
