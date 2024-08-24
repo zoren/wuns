7c5fae579554483cbefbaf8a6cae2cf86176230b
Søren Sjørup <soren@zoren.dk>
2024-08-24 12:34:55 +0200

    clean up js instructions

--- esm/instructions.js
+++ esm/instructions.js
@@ -1,29 +1,6 @@
-import { isSigned32BitInteger } from './core.js'
-
-const instructions = {}
-const empty = Object.freeze([])
-const u32 = 'u32'
-const s32 = 's32'
-const s64 = 's64'
-const i32 = 'i32'
-
-const i32i32 = Object.freeze([i32, i32])
-const mkZeroImm = (func) => {
-  if (func.length !== 2) throw new Error('expected 2 params: ' + func.length)
-  return Object.freeze({
-    immediateParams: empty,
-    params: i32i32,
-    func: () => func,
-  })
-}
-const addI32InstructionObj = (name, obj) => {
-  instructions[name] = obj
-  instructions['i32.' + name] = obj
-}
+const instructionFunctions = {}
 const addI32InstructionFunc = (name, f) => {
-  const obj = mkZeroImm(f)
-  instructions[name] = obj
-  instructions['i32.' + name] = obj
+  instructionFunctions['i32.' + name] = f
 }
 const i32binops = {
   add: '+',
@@ -41,9 +18,8 @@ const i32binops = {
   'ge-s': '>=',
 }
 
-for (const [name, op] of Object.entries(i32binops)) {
+for (const [name, op] of Object.entries(i32binops))
   addI32InstructionFunc(name, Function('a', 'b', `return (a ${op} b) | 0`))
-}
 
 const i32BitwiseOps = [
   { name: 'and', op: '&', alias: 'bitwise-and' },
@@ -59,58 +35,10 @@ for (const { name, op, alias } of i32BitwiseOps) {
   addI32InstructionFunc(alias, f)
 }
 
-addI32InstructionObj('const', {
-  immediateParams: [s32],
-  params: [],
-  func: (val) => {
-    const normalised = val | 0
-    if (val !== normalised) throw new Error('invalid immediate value')
-    // return the normalized value as -0 === 0
-    return () => normalised
-  },
+instructionFunctions['unreachable'] = Object.freeze(() => {
+  throw new Error('unreachable')
 })
-instructions['i64.const'] = {
-  immediateParams: [s64],
-  params: [],
-  func: (val) => {
-    const bigInt = BigInt(val)
-    return () => bigInt
-  },
-}
-instructions['unreachable'] = Object.freeze({
-  immediateParams: empty,
-  params: empty,
-  func: () => () => {
-    throw new Error('unreachable')
-  },
-})
-// The target memory and source segment are given as immediates.
-// The instruction has the signature [i32 i32 i32] -> []. The parameters are, in order:
 
-// top-2: destination address
-// top-1: offset into the source segment
-// top-0: size of memory region in bytes
-// instructions['data.passive']
-const typedArrayCtorByByteSizeSigned = (byteSize, signed) => {
-  switch (byteSize) {
-    case 1:
-      return signed ? Int8Array : Uint8Array
-    case 2:
-      return signed ? Int16Array : Uint16Array
-    case 4:
-      return signed ? Int32Array : Uint32Array
-    default:
-      throw new Error('unsupported byte size')
-  }
-}
-
-Object.freeze(instructions)
-
-const instructionFunctions = {}
-for (const [name, f] of Object.entries(instructions)) {
-  if (f.immediateParams.length !== 0) continue
-  instructionFunctions[name] = f.func()
-}
 Object.freeze(instructionFunctions)
 
-export { instructions, instructionFunctions }
+export { instructionFunctions }
