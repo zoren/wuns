764698e73d7647d7cd9230194af4808102b00cbe
Søren Sjørup <soren@zoren.dk>
2024-08-26 09:28:21 +0200

    merge testers

--- esm/test-expand-check.js
+++ esm/test-expand-check.js
@@ -1,14 +1,38 @@
-import { makeInitContext, parseEvalFiles, runCform } from './interpreter.js'
+import { parseFile } from './parseTreeSitter.js'
 import { makeGetDefVarValue } from './core.js'
-const { compile } = makeInitContext()
+import { makeInitContext, parseEvalFiles, runCform } from './interpreter.js'
+{
+  const { compile } = makeInitContext()
 
-parseEvalFiles(
-  compile,
-  ['std3', 'wasm-instructions', 'macro-expand', 'check'].map((name) => `../wuns/${name}.wuns`),
-)
+  const files = ['std3', 'wasm-instructions', 'macro-expand'].map((f) => `../wuns/${f}.wuns`)
+  parseEvalFiles(compile, files)
+  const getDefVarValue = makeGetDefVarValue(compile)
 
-const getDefVarVal = makeGetDefVarValue(compile)
-const testCheck = getDefVarVal('test-check')
-runCform(() => {
-  testCheck()
-})
+  const testExpand = getDefVarValue('test-expand')
+
+  runCform(() => {
+    testExpand()
+  })
+
+  const testExpandNoErrors = getDefVarValue('test-expand-no-errors-fn')
+
+  runCform(() => {
+    testExpandNoErrors(
+      ['std3', 'wasm-instructions', 'macro-expand', 'check'].flatMap((f) => parseFile(`../wuns/${f}.wuns`)),
+    )
+  })
+}
+{
+  const { compile } = makeInitContext()
+
+  parseEvalFiles(
+    compile,
+    ['std3', 'wasm-instructions', 'macro-expand', 'check'].map((name) => `../wuns/${name}.wuns`),
+  )
+
+  const getDefVarVal = makeGetDefVarValue(compile)
+  const testCheck = getDefVarVal('test-check')
+  runCform(() => {
+    testCheck()
+  })
+}
