fa72878a71a8a273f5a322ce39b4e6f86f9f5fc2
Søren Sjørup <soren@zoren.dk>
2024-08-31 19:08:44 +0200

    improve parameter names

--- esm/host.js
+++ esm/host.js
@@ -19,13 +19,13 @@ export const apply = (fn, args) => {
   return fn(...args)
 }
 
-export const is_word = (f) => isWord(f)
+export const is_word = (form) => isWord(form)
 
-export const is_list = (f) => isList(f)
+export const is_list = (form) => isList(form)
 export { meta }
 
-export const word_with_meta = (w, meta_data) => wordWithMeta(wordValue(w), meta_data)
-export const list_with_meta = (l, meta_data) => listWithMeta(l, meta_data)
+export const word_with_meta = (word, meta_data) => wordWithMeta(wordValue(word), meta_data)
+export const list_with_meta = (list, meta_data) => listWithMeta(list, meta_data)
 
 export const var_meta = (v) => {
   if (!isDefVar(v)) throw new Error('var-meta, not a defvar: ' + v)
@@ -35,24 +35,25 @@ export const var_get = (v) => {
   if (!isDefVar(v)) throw new Error('not a defvar: ' + v)
   return v.value
 }
-export const word_byte_size = (w) => {
-  if (isWord(w)) return wordValue(w).length
-  throw new Error('word-byte-size expects word, found: ' + w + ' ' + typeof w)
+export const word_byte_size = (word) => {
+  if (isWord(word)) return wordValue(word).length
+  throw new Error('word-byte-size expects word, found: ' + word + ' ' + typeof word)
 }
 
-export const char_code_at = (w, i) => {
-  if (!isWord(w)) throw new Error('char-code-at expects word')
-  if (!isSigned32BitInteger(i)) throw new Error('char-code-at expects number: ' + i)
-  const len = wordValue(w).length
-  if (i < -len || i >= len) throw new Error('index out of bounds: ' + i + ' ' + len)
-  return String(w).at(i).charCodeAt(0)
+export const char_code_at = (word, index) => {
+  if (!isWord(word)) throw new Error('char-code-at expects word')
+  if (!isSigned32BitInteger(index)) throw new Error('char-code-at expects number: ' + index)
+  const len = wordValue(word).length
+  if (index < -len || index >= len) throw new Error('index out of bounds: ' + index + ' ' + len)
+  return String(word).at(index).charCodeAt(0)
 }
-export const concat_words = (w1, w2) => word(wordValue(w1) + wordValue(w2))
-export const char_code_to_word = (cp) => word(String.fromCodePoint(cp))
+export const concat_words = (word_1, word_2) => word(wordValue(word_1) + wordValue(word_2))
+// todo rename
+export const char_code_to_word = (code_point) => word(String.fromCodePoint(code_point))
 
-export const size = (a) => {
-  if (isList(a)) return a.length
-  throw new Error('size expects list, found: ' + a + ' ' + typeof a)
+export const size = (list) => {
+  if (isList(list)) return list.length
+  throw new Error('size expects list, found: ' + list + ' ' + typeof list)
 }
 const symbolListGrowable = Symbol.for('wuns-list-growable')
 const symbolListMutable = Symbol.for('wuns-list-mutable')
@@ -63,16 +64,16 @@ export const growable_list = () => {
   l[symbolListGrowable] = true
   return l
 }
-export const clone_growable_to_frozen_list = (l) => {
-  if (!isList(l)) throw new Error('freeze-growable-list expects list')
-  if (!isGrowable(l)) throw new Error('freeze-growable-list expects growable list')
-  return makeList(...l)
+export const clone_growable_to_frozen_list = (growable_list) => {
+  if (!isList(growable_list)) throw new Error('freeze-growable-list expects list')
+  if (!isGrowable(growable_list)) throw new Error('freeze-growable-list expects growable list')
+  return makeList(...growable_list)
 }
-export const push = (ar, e) => {
-  if (!isList(ar)) throw new Error('push expects array')
-  if (!isGrowable(ar)) throw new Error('push expects growable list')
-  if (Object.isFrozen(ar)) throw new Error('push expects mutable array')
-  ar.push(e)
+export const push = (growable_list, element) => {
+  if (!isList(growable_list)) throw new Error('push expects array')
+  if (!isGrowable(growable_list)) throw new Error('push expects growable list')
+  if (Object.isFrozen(growable_list)) throw new Error('push expects mutable array')
+  growable_list.push(element)
 }
 export const mutable_list_of_size = (size) => {
   if (size < 0) throw new Error('mutable-list-of-size expects non-negative size')
@@ -81,68 +82,62 @@ export const mutable_list_of_size = (size) => {
   return l
 }
 
-export const freeze_mutable_list = (l) => {
-  if (!isList(l)) throw new Error('freeze-mutable-list expects array')
-  if (!isMutable(l)) throw new Error('freeze-mutable-list expects mutable list')
-  delete l[symbolListMutable]
-  Object.freeze(l)
+export const freeze_mutable_list = (mutable_list) => {
+  if (!isList(mutable_list)) throw new Error('freeze-mutable-list expects array')
+  if (!isMutable(mutable_list)) throw new Error('freeze-mutable-list expects mutable list')
+  delete mutable_list[symbolListMutable]
+  Object.freeze(mutable_list)
 }
-export const set_array = (ar, i, e) => {
+export const set_array = (ar, index, element) => {
   if (!isList(ar)) throw new Error('set-array expects array')
   if (!isMutable(ar)) throw new Error('set-array expects mutable list')
   if (Object.isFrozen(ar)) throw new Error('set-array expects mutable array')
-  if (!isSigned32BitInteger(i)) throw new Error('set-array expects integer index')
-  if (i < 0 || i >= ar.length) throw new Error('set-array index out of bounds: ' + i + ' ' + ar.length)
-  ar[i] = e
+  if (!isSigned32BitInteger(index)) throw new Error('set-array expects integer index')
+  if (index < 0 || index >= ar.length) throw new Error('set-array index out of bounds: ' + index + ' ' + ar.length)
+  ar[index] = element
 }
-export const at = (v, i) => {
-  if (!isList(v)) throw new Error('at expects list, got ' + typeof v + ' ' + v)
-  if (!isSigned32BitInteger(i)) throw new Error('at expects number: ' + i)
-  const len = v.length
-  if (i < -len || i >= len) throw new Error('index out of bounds: ' + i + ' ' + len)
-  return v.at(i)
+export const at = (list, index) => {
+  if (!isList(list)) throw new Error('at expects list, got ' + typeof list + ' ' + list)
+  if (!isSigned32BitInteger(index)) throw new Error('at expects number: ' + index)
+  const len = list.length
+  if (index < -len || index >= len) throw new Error('index out of bounds: ' + index + ' ' + len)
+  return list.at(index)
 }
 
 export { atom }
-export const is_atom = (a) => isAtom(a)
-export const atom_get = (a) => {
-  if (!isAtom(a)) throw new Error('not an atom: ' + a)
-  return a.value
+export const is_atom = (atom) => isAtom(atom)
+export const atom_get = (atom) => {
+  if (!isAtom(atom)) throw new Error('not an atom: ' + atom)
+  return atom.value
 }
-export const atom_set = (a, v) => {
-  if (!isAtom(a)) throw new Error('not an atom: ' + a)
-  a.value = v
+export const atom_set = (atom, value) => {
+  if (!isAtom(atom)) throw new Error('not an atom: ' + atom)
+  atom.value = value
 }
-export const is_identical = (a, b) => a === b
+export const is_identical = (value_a, value_b) => value_a === value_b
 
 // https://stackoverflow.com/a/69745650/3495920
 const isPlainObject = (value) => value?.constructor === Object
 
 export const transient_kv_map = () => ({})
-export const has = (m, k) => {
-  if (!isPlainObject(m)) throw new Error('has expects map')
-  return wordValue(k) in m
+export const has = (kv_map, key) => {
+  if (!isPlainObject(kv_map)) throw new Error('has expects map')
+  return wordValue(key) in kv_map
 }
-export const get = (m, k) => {
-  if (!isPlainObject(m)) throw new Error('get expects map')
-  const ks = wordValue(k)
-  if (ks in m) return m[ks]
-  throw new Error(`key not found: ${ks} in [${Object.keys(m)}]`)
+export const get = (kv_map, key) => {
+  if (!isPlainObject(kv_map)) throw new Error('get expects map')
+  const ks = wordValue(key)
+  if (ks in kv_map) return kv_map[ks]
+  throw new Error(`key not found: ${ks} in [${Object.keys(kv_map)}]`)
 }
-export const keys = (m) => {
-  if (!isPlainObject(m)) throw new Error('keys expect map')
-  return makeList(...Object.keys(m).map(word))
+export const keys = (kv_map) => {
+  if (!isPlainObject(kv_map)) throw new Error('keys expect map')
+  return makeList(...Object.keys(kv_map).map(word))
 }
 export const set_kv_map = (kv_map, key, value) => {
-  if (!isPlainObject(kv_map)) throw new Error('set expect map')
-  if (Object.isFrozen(kv_map)) throw new Error('set expects mutable object')
+  if (!isPlainObject(kv_map)) throw new Error('set-kv-map expect map')
+  if (Object.isFrozen(kv_map)) throw new Error('set-kv-map expects mutable object')
   kv_map[wordValue(key)] = value
-}
-// todo remove this
-export const delete_key = (o, k) => {
-  if (!isPlainObject(o)) throw new Error('delete-key expect map')
-  if (Object.isFrozen(o)) throw new Error('delete expects mutable object')
-  delete o[wordValue(k)]
 }
 export const freeze_kv_map = (o) => {
   if (!isPlainObject(o)) throw new Error('keys expect map')
@@ -150,7 +145,7 @@ export const freeze_kv_map = (o) => {
   Object.freeze(o)
 }
 
-export const log = (x) => console.log(print(x))
+export const log = (form) => console.log(print(form))
 export const concat = (...lists) => {
   const l = []
   for (const list of lists) {
