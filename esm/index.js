602a23fec0a7603af9908a0d5a241d2c47d86540
Søren Sjørup <soren@zoren.dk>
2024-09-16 08:39:36 +0200

    rf index.js

--- esm/index.js
+++ esm/index.js
@@ -1,6 +1,3 @@
-import fs from 'node:fs'
-import * as readline from 'node:readline'
-import { stdin, nextTick, stdout } from 'node:process'
 import { makeHost } from './host-simulated-mem.js'
 import { jsHost } from './host-js.js'
 import { makeInitContext } from './interpreter.js'
@@ -10,48 +7,19 @@ const host = jsHost
 
 const { parseEvalFiles, parseStringToForms, evalLogForms } = makeInitContext(host)
 const commandLineArgs = process.argv.slice(2)
+const endsWithDashFlag = commandLineArgs.at(-1) === '-'
+const files = endsWithDashFlag ? commandLineArgs.slice(0, -1) : commandLineArgs
 
-parseEvalFiles(commandLineArgs)
+parseEvalFiles(files)
 
-const historyFilePath = 'history.json'
+import { startRepl } from './repl-util.js'
 
-let history = []
-try {
-  const histO = JSON.parse(fs.readFileSync(historyFilePath, 'utf8'))
-  history = histO.history
-} catch (err) {
-  if (err.code !== 'ENOENT') throw err
-}
-
-const rl = readline.createInterface({
-  input: stdin,
-  output: stdout,
-  terminal: true,
-  historySize: 1024,
-  history,
-  removeHistoryDuplicates: true,
-  tabSize: 2,
-})
-
-rl.on('history', (history) => {
-  const historyObject = { history, date: new Date().toISOString() }
-  fs.writeFileSync(historyFilePath, JSON.stringify(historyObject))
-})
-
-const prompt = () => {
-  rl.question(`wuns> `, (line) => {
-    if (line === '') {
-      console.log(`Bye!`)
-      rl.close()
-      return
-    }
+if (!endsWithDashFlag) {
+  startRepl('history.json', 'wuns> ', (line) => {
     try {
       evalLogForms(parseStringToForms(line))
     } catch (err) {
       console.error(err)
     }
-    nextTick(prompt)
   })
 }
-
-prompt()
