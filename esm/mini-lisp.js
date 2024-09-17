3d269a3941c613f412b53b250912d2154d04bb73
Søren Sjørup <soren@zoren.dk>
2024-09-17 08:44:53 +0200

    use typeable forms in mini-lisp

--- esm/mini-lisp.js
+++ esm/mini-lisp.js
@@ -63,56 +63,64 @@ const makeParamEnv = (defFunc, args) => {
 }
 
 const getMeta = (form) => form[symbolMeta]
+import { tryGetFormWord, tryGetFormList } from './core.js'
 
 const makeEvaluator = (externObj) => {
   const defEnv = new Map()
   const go = (env, form) => {
     while (true) {
-      if (typeof form === 'string') {
+      const word = tryGetFormWord(form)
+      if (word) {
         let curEnv = env
         while (curEnv) {
-          if (curEnv.has(form)) return curEnv.get(form)
+          if (curEnv.has(word)) return curEnv.get(word)
           curEnv = curEnv.outer
         }
-        if (!defEnv.has(form)) throw new Error('undefined variable: ' + form)
-        return defEnv.get(form)
+        if (!defEnv.has(word)) throw new Error('undefined variable: ' + word)
+        return defEnv.get(word)
       }
-      if (!Array.isArray(form) || form.length === 0) throw new Error('unexpected form: ' + print(form))
-      const [first] = form
-      switch (first) {
+      const list = tryGetFormList(form)
+      if (!list) throw new Error('unexpected form: ' + form)
+      const [firstForm] = list
+      const firstWord = tryGetFormWord(firstForm)
+      switch (firstWord) {
         case 'i32':
-          return +form[1] | 0
+          return +list[1] | 0
         case 'word':
-          return form[1]
-        case 'func':
-          return makeClosure(env, ...form.slice(1))
+          return tryGetFormWord(list[1])
+        case 'func': {
+          const name = tryGetFormWord(list[1])
+          const parameters = tryGetFormList(list[2]).map(tryGetFormWord)
+          const body = list[3]
+          return makeClosure(env, name, parameters, body)
+        }
         case 'extern': {
           let ext = externObj
-          for (let i = 1; i < form.length; i++) {
-            ext = ext[form[i]]
-            if (ext === undefined) throw new Error('undefined extern: ' + form.slice(1, i + 1).join(' '))
+          for (let i = 1; i < list.length; i++) {
+            ext = ext[list[i]]
+            if (ext === undefined) throw new Error('undefined extern: ' + list.slice(1, i + 1).join(' '))
           }
           return ext
         }
 
         case 'if':
-          form = form[go(env, form[1]) ? 2 : 3]
+          form = list[go(env, list[1]) ? 2 : 3]
           continue
         case 'do':
-          if (form.length === 1) return
-          for (let i = 1; i < form.length - 1; i++) go(env, form[i])
-          form = form.at(-1)
+          if (list.length === 1) return
+          for (let i = 1; i < list.length - 1; i++) go(env, list[i])
+          form = list.at(-1)
           continue
         case 'let': {
-          const bindings = form[1]
+          const bindings = list[1]
           const newEnv = makeEnv(env)
           for (let i = 0; i < bindings.length - 1; i += 2) setEnv(newEnv, bindings[i], go(newEnv, bindings[i + 1]))
           env = newEnv
-          form = form[2]
+          form = list[2]
           continue
         }
         case 'recur': {
-          const eargs = form.slice(1).map((arg) => go(env, arg))
+          const eargs = list.slice(1).map((arg) => go(env, arg))
           const { closure, paramEnv } = env.invocation
           const { parameters, body } = closure
           setParameters(parameters, paramEnv, eargs)
@@ -120,14 +128,14 @@ const makeEvaluator = (externObj) => {
           continue
         }
         case 'def': {
-          const [, name, value] = form
+          const [, name, value] = list
           defEnv.set(name, go(env, value))
           return
         }
       }
-      const args = form.slice(1)
-      if (typeof first === 'string' && !hasLocal(env, first)) {
-        const defFunc = defEnv.get(first)
+      const args = list.slice(1)
+      if (firstWord && !hasLocal(env, firstWord)) {
+        const defFunc = defEnv.get(firstWord)
         if (defFunc instanceof Closure) {
           const funcKind = getMeta(defFunc)
           if (funcKind === 'fexpr') return go(makeParamEnv(defFunc, args), defFunc.body)
@@ -138,10 +146,10 @@ const makeEvaluator = (externObj) => {
         }
       }
       {
-        const func = go(env, first)
+        const func = go(env, firstForm)
         const eargs = args.map((arg) => go(env, arg))
         if (typeof func === 'function') return func(...eargs)
-        if (!(func instanceof Closure)) throw new Error('not a function: ' + print(first))
+        if (!(func instanceof Closure)) throw new Error('not a function: ' + print(firstForm))
         env = makeParamEnv(func, eargs)
         form = func.body
         continue
@@ -151,6 +159,8 @@ const makeEvaluator = (externObj) => {
   return (form) => go(makeEnv(), form)
 }
 
+import { formWord, formList } from './core.js'
+
 import { parse } from './parseTreeSitter.js'
 
 function* parseToForms(content, metaPrefix) {
@@ -159,17 +169,15 @@ function* parseToForms(content, metaPrefix) {
    * @param {TSParser.SyntaxNode} node
    */
   const nodeToForm = (node) => {
-    const { isError, type } = node
+    const { isError, type, startPosition } = node
     if (isError) throw new Error('unexpected error node')
+    const { row, column } = startPosition
+    const metaData = makeMeta(row + 1, column + 1)
     switch (type) {
       case 'word':
-        return node.text
-      case 'list': {
-        const l = node.namedChildren.map(nodeToForm)
-        const { row, column } = node.startPosition
-        l[symbolMeta] = makeMeta(row + 1, column + 1)
-        return Object.freeze(l)
-      }
+        return formWord(node.text, metaData)
+      case 'list':
+        return formList(Object.freeze(node.namedChildren.map(nodeToForm)), metaData)
       default:
         throw new Error('unexpected node type: ' + type)
     }
