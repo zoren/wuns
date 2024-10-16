4cb1983f30c9575a5a8c3fe1876799887e3a9079
Søren Sjørup <soren@zoren.dk>
2024-10-16 10:26:43 +0200

    fix externs

--- esm/runtime-lib/externs.js
+++ esm/runtime-lib/externs.js
@@ -2,11 +2,11 @@ import { wrapJSFunctionsToObject } from '../utils.js'
 
 const wrap = async (filename) => wrapJSFunctionsToObject(Object.values(await import(filename)))
 
-const host = wrap('./host.js')
+const host = await wrap('./host.js')
 
 const performance_now = () => performance.now()
 
-const js = wrap('./js-utils.js')
+const js = await wrap('./js-utils.js')
 
 const externs = {
   host,
