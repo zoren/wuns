0907eba2db4e2d34d2f3494143dd7f0b93475be1
Søren Sjørup <soren@zoren.dk>
2024-12-07 23:56:26 +0100

    make wabt.js dump source on fail

--- esm/runtime-lib/wabt.js
+++ esm/runtime-lib/wabt.js
@@ -4,21 +4,29 @@ const wabt = await wabtProm()
 const wat_to_byte_array = (inputBuffer) => {
   if (!(inputBuffer instanceof Uint8Array)) throw new Error('expects Uint8Array')
   // print as string
-  // const inputString = new TextDecoder().decode(inputBuffer)
-  // console.log({ inputString })
-  const module = wabt.parseWat('', inputBuffer, {
-    multi_memory: true,
-  })
-  module.resolveNames()
-  // todo maybe do this for debug symbols?
-  // module.generateNames()
-  module.validate()
-  const { buffer, log } = module.toBinary({
-    // log: true,
-    write_debug_names: true,
-  })
-  // console.log({ toBinaryLog: log })
-  return buffer
+  try {
+    const wasmFeatures = {
+      multi_memory: true,
+      memory64: true,
+    }
+    const module = wabt.parseWat('', inputBuffer, wasmFeatures)
+    module.resolveNames()
+    // todo maybe do this for debug symbols?
+    // module.generateNames()
+    // warning the types for wabt does not reflect that validate also needs wasmFeatures
+    // todo report this to wabt
+    module.validate(wasmFeatures)
+    const { buffer, log } = module.toBinary({
+      // log: true,
+      write_debug_names: true,
+    })
+    // console.log({ toBinaryLog: log })
+    return buffer
+  } catch (e) {
+    console.error(e)
+    console.error(new TextDecoder().decode(inputBuffer))
+    throw e
+  }
 }
 
 export { wat_to_byte_array as 'wat-to-byte-array' }
